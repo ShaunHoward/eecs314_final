70655f27e4450ef36228b7849777bd287dcc5f4a
ShaunHoward <smh150@case.edu>
2015-04-16 04:38:21 -0400

    Making progress toward saturation filter

--- photoFilter.asm
+++ photoFilter.asm
@@ -1,5 +1,8 @@
 .data
 welcome:	.asciiz	"Welcome! Please enter the name of the file you would like to edit?  \n"
+filterType:     .asciiz "Please enter the filter you would like to use.\nA list of filters" +
+ 		"are the following:\n0: Saturation, 1: Grayscale, 2: Edge-detection, 3: Brightness, 4: Hue\n"
+filterPercent:    .asciiz "Now enter the percentage you want to wish to filter the image from 0 to 100\n"
 header: 	.space   54 	# bitmap header data stored here 
 inputFileName:	.space	128 	# name of the input file, specified by user
 outname: 	.asciiz  "OUTPUT_IMAGE.bmp"
@@ -32,6 +35,7 @@ main:
 	li		$t0, '\n'		# we are looking for this character
 	li		$t1, 128		# length of the inputFileName
 	li		$t2, 0			# clear the current character
+	
 newLineLoop:
 	beqz		$t1, newLineLoopEnd	# if end of string, jump to loop end
 	subu		$t1, $t1, 1		# decrement the index
@@ -78,6 +82,31 @@ newLineLoopEnd:
 	li 		$v0, 10			# syscall 10, exit
 	syscall
 	
+read_filter_data:
+        #print filter type string
+	li		$v0, 4			# syscall 4, print string
+	la		$a0, filterType		# load filter selection string
+	syscall
+	
+        #read filter type
+	li		$v0, 5			# syscall 5, read integer (0 to 4)
+	syscall
+	
+	#store filter type in $t4
+	addi            $t4, $v0, 0      
+	
+	#print filter percentage string
+	li		$v0, 4			# syscall 4, print string
+	la		$a0, filterPercent	# load filter selection string
+	syscall
+	
+        #read filter percentage
+	li		$v0, 5			# syscall 5, read integer (0 to 100)
+	syscall
+	
+	#store filter percentage in $t5
+	addi            $t5, $v0, 0         
+	
 	#************ NOW THE IMAGE IS IN AN ARRAY STARTING AT $S2 **********#
 	
 ##########################################################################################
@@ -102,9 +131,51 @@ filter_init:
 	beq 4, $t4, hue 
 	
 saturation:
+ 
 	#saturate each r g b value based on percentage given
 	#min value is 0, max value is 255, use fraction of each value for new value
+	#use base index for 3 values, have constants to determine bit offsets of R,G,B
+        #counter starts at 0, equations like Bi = i + B, Gi = i + G, Ri = i + R
+        #the idea here is to use srl and sll, dumping excess 1's from the MSB
+	#load the current blue pixel value into $t6
+	lb $t6, $t7($s2)
+
+sat_loop:
+	#normalize the percentage to 0 - 255 scale
+	mult $t5, $t5, 255
+	div $t5, $t5, 100
 	
+	#increment the pixel's value
+	add $t6, $t6, $t5
+	
+	#do this for now, can add loop to add each bit to a register and then write that 
+	#at end but want to test theory first
+	sb $t6, $t7($s2)
+	
+        #increment counter for pixel saturation loop
+        addi $t7, $t7, 1
+        
+        #find what pixel we are at currently
+        div $t3, $t7, 3
+        
+        #check if at the end of the pixel array
+        #exit when at end
+        beq $s1, $t3, exit
+	
+	#otherwise, jump to saturate next pixel
+	j sat_loop
+	
+	#compare $t6 with $t5, 
+	
+        #figure out the current value's percentage out of 256
+	#divu $t3, $t6, 255
+	
+	#find difference between inputPercentage and currPercentage in percentages
+	#sub $t3, $t5, $t3
+	
+        #normalize the percentage to 0 - 255 scale
+	#mult $t6, $t6, 255
+
 grayscale:
 	#convert colors into grayscale
 	
@@ -115,6 +186,8 @@ brightness:
 	#modify rgb values
 	
 hue:
+
+exit:
 
 write_file:
 	
